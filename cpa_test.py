5e85729fe79f8325f9359de1bae6ac865c6bfd73
yodalee <lc85301@gmail.com>
2018-01-21 16:40:15 +0800

    add cpatest

--- cpa_test.py
+++ cpa_test.py
@@ -1,21 +1,20 @@
 import unittest
 import numpy as np
 
-from cpa import npCalCorr
+from utils import Utils
 
 THRESHOLD = 1e-6
 
 class TestCorrelation(unittest.TestCase):
-    def isEqual(self, x, y):
-        """If difference of x and y < 1e-6, judge as equal"""
-        return abs(x - y) < THRESHOLD
+    def setUp(self):
+        self.utils = Utils()
 
     def testCorrelation(self):
-        X = np.random.rand(10000) * 2 - 1
-        Y = np.random.rand(10000) * 2 - 1
+        X = np.random.rand(10) * 2 - 1
+        Y = np.random.rand(10) * 2 - 1
         ans = np.corrcoef(X, Y)[0][1]
-        ret = npCalCorr(X, Y)
-        return self.isEqual(ans, ret)
+        ret = self.utils.cuCalCorr(X, Y)
+        self.assertAlmostEqual(ans, ret)
 
 if __name__ == "__main__":
     unittest.main()
